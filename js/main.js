d2c654cb090dc49f97c553bb317985cdad12f24a
Nikita <lisenkovandrei@yandex.ru>
2022-10-20 23:33:49 +0400

    9.8. Помощь друга modified:   index.html new file:   js/change-photo.js modified:   js/form.js modified:   js/main.js new file:   js/slider.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 import {validateData} from './form.js';
 import {renderPhoto} from './rendering.js';
+import {writeTargetScale} from './change-photo.js';
+import {onFilterChange} from './slider.js';
 
 renderPhoto();
 console.log(validateData);
+writeTargetScale();
+onFilterChange();
