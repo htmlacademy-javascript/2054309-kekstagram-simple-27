403f48bff4d54856d33470b5365f29283a556a57
Keks <keks@htmlacademy.ru>
2022-10-23 13:59:17 +0300

    :heavy_check_mark: Сборка #8

--- 8/js/change-photo.js
+++ 8/js/change-photo.js
@@ -7,47 +7,48 @@ const scaleValue = document.querySelector('.scale__control--value');
 const imageContainer = document.querySelector('.img-upload__preview');
 const effectsList = document.querySelector('.img-upload__effects');
 
-let imageScaleValue = 100;
+//let previousClass;
 
-const writeTargetScale = () => {
+const writeTargetScale = (imageScaleValue) => {
   scaleValue.value = `${imageScaleValue}%`;
   const scalePercent = imageScaleValue / 100;
   imageContainer.style.transform = `scale(${scalePercent})`;
 };
 
-controlUploadFile.addEventListener('change', reloadPhoto);
+const initChangePhoto = () => {
+  let imageScaleValue = 100;
+  controlUploadFile.addEventListener('change', reloadPhoto);
 
-function reloadPhoto () {
+  biggerButton.addEventListener('click', () => {
+    if (imageScaleValue < 100 && imageScaleValue >= 25) {
+      imageScaleValue += 25;
+      writeTargetScale(imageScaleValue);
+    }
+  });
+
+  smallerButton.addEventListener('click', () => {
+    if (imageScaleValue <= 100 && imageScaleValue > 25) {
+      imageScaleValue -= 25;
+      writeTargetScale(imageScaleValue);
+    }
+  });
+
+  let previousClass;
+
+  effectsList.addEventListener('change', (evt) => {
+    const imageCore = imageContainer.querySelector('img');
+    imageCore.classList.remove(previousClass);
+    const imageEditedClass = `effects__preview--${evt.target.value}`;
+    imageCore.classList.add(imageEditedClass);
+    previousClass = imageEditedClass;
+  });
+
+  effectsList.addEventListener('change', onFilterChange);
+};
+
+function reloadPhoto (imageScaleValue) {
   imageScaleValue = 100;
-  writeTargetScale();
+  writeTargetScale(imageScaleValue);
 }
 
-biggerButton.addEventListener('click', () => {
-  if (imageScaleValue < 100 && imageScaleValue >= 25) {
-    imageScaleValue += 25;
-    writeTargetScale();
-  }
-});
-
-smallerButton.addEventListener('click', () => {
-  if (imageScaleValue <= 100 && imageScaleValue > 25) {
-    imageScaleValue -= 25;
-    writeTargetScale();
-  }
-});
-
-//Реализация применения фильтров
-
-let previousClass;
-effectsList.addEventListener('change', (evt) => {
-  const imageCore = imageContainer.querySelector('img');
-  imageCore.classList.remove(previousClass);
-  const imageEditedClass = `effects__preview--${evt.target.value}`;
-  imageCore.classList.add(imageEditedClass);
-  previousClass = imageEditedClass;
-});
-
-effectsList.addEventListener('change', onFilterChange);
-
-
-export {writeTargetScale};
+export {writeTargetScale, initChangePhoto};
